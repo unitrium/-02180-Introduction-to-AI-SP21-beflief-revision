f31da3dd08907e7801395b000e0c3b795e9a9b9e
Adin1128 <Adin11281128@hotmail.com>
2021-04-27 11:28:03 +0200

    Comments and while

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from code.agent import BeliefBase, Agent
 
+
 if __name__ == "__main__":
     bb = BeliefBase()
-    
+    while True:
+        print("The current belief base entails"+"Cn{"+"a->b"+","+"a"+"}")
+        var = input("Please add to the belief base: ")
+        
+        #   If the input is quit break (adin)
+        #   use sympy to check if the var contains the correct language (adin)
+        #   if the language is correct convert the belief base into sympy and do revision.
+        
+        #   Check entailment with negated belief to check if the belief base contradicts your new belief.
+        #   if it isn't entailed contract from the belief base then add belief.
+        #   There shouldn't be any duplicate beliefs.
