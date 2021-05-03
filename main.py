e65b6895957ecd92199866a386aa32b1efb11ef1
Adin1128 <Adin11281128@hotmail.com>
2021-05-03 12:27:00 +0200

    Quit and validation of input

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from code.agent import BeliefBase
-
+from sympy.logic.boolalg import to_cnf, And, Or, Not
 
 if __name__ == "__main__":
     bb = BeliefBase()
@@ -7,9 +7,12 @@ if __name__ == "__main__":
         print('Current belief base:')
         bb.display_belief()
         var = input("Please add to the belief base: ")
-        
-        #   If the input is quit break (adin)
-        #   use sympy to check if the var contains the correct language (adin)
+        if var.lower() == "quit" :  #   If the input is quit break (adin)
+            break
+        try:   #   use sympy to check if the var contains the correct language (adin)
+            to_cnf(var)
+        except:
+            print("The input is invalid.")    
         #   if the language is correct convert the belief base into sympy and do revision.
         
         #   Check entailment with negated belief to check if the belief base contradicts your new belief.
