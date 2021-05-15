3351f50c8a92d5276e5648251f204bbcf7b234b1
Adin1128 <Adin11281128@hotmail.com>
2021-05-15 14:35:38 +0200

    tests comments

--- code/tests/basic_test.py
+++ code/tests/basic_test.py
@@ -14,25 +14,41 @@ def test_revision():
     assert len(agent.belief_base.beliefBase) == 2
 
 
+#if we try to remove belief from base the belief none of the beliefs in base will entail the belief after contraction.
 def test_contraction_closure():
+    #knowledge base is p, p->q,r add ~r : Cn(p,p->q)
+    
     pass
 
-
+#if the belief 
 def test_contraction_success():
+    #knowledge base is p, p->q,r contract r : Cn(p,p->q)
+    #pass if r isn't in the belief base
+    
+    
     pass
 
 
 def test_contraction_inclusion():
+    #knowledge base is p, p->q,r contract r : Cn(p,p->q)
+    #pass if there are no new beliefs right?
     pass
 
 
 def test_contraction_vacuity():
+    #knowledge base is p, p->q add ~r : Cn(p,p->q)
+    #pass if there is no change.
     pass
 
 
 def test_contraction_extensionality():
+    #do 10 times
+    #knowledge base is p, p<->q,r contract p<->q : Cn(p,r)
+    #do once knowledge base is p, p<->q,r contract p->q & q->p : Cn(p,r)
+    #pass if all created belief bases are equivalent
     pass
 
 
 def test_contraction_recovery():
+    #how would i test this?
     pass
