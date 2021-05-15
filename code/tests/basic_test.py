0ccaba0c7cfc8c22489bfc886204121a13123c46
Adin1128 <Adin11281128@hotmail.com>
2021-05-15 19:52:14 +0200

    tests contraction

--- code/tests/basic_test.py
+++ code/tests/basic_test.py
@@ -2,6 +2,8 @@
 from ..agent.agent import Agent
 from ..agent.world import World, Worlds
 from ..agent.beliefbase import Belief
+from ..agent.beliefbase import BeliefBase
+from sympy.logic.boolalg import to_cnf, Not
 
 
 def test_basic_create():
@@ -11,9 +13,9 @@ def test_basic_create():
 
 def test_revision():
     agent = Agent([('p'), ('q'), ('p>>q')])
-    assert len(agent.belief_base.beliefBase.keys()) == 2
+    assert len(agent.belief_base.beliefBase.keys()) == 3
     agent.belief_base.revise('~q')
-    assert len(agent.belief_base.beliefBase) == 3
+    assert len(agent.belief_base.beliefBase) == 2
 
 
 #if we try to remove belief from base, none of the beliefs in base will entail the belief after contraction.
@@ -32,12 +34,7 @@ def test_contraction_success():
     agent = Agent([('p'),('q'), ('p>>q'),('r')])
     new_belief = Belief('r')
     agent.belief_base.contract(new_belief)
-    a : bool
-    a = true
-    for key, belief in agent.belief_base.beliefBase:
-        if key=='r':
-            a = false   
-    assert a
+    assert 'r' not in agent.belief_base.beliefBase
 
 #check that the result is a subset of the original beliefbase.
 def test_contraction_inclusion():
@@ -45,9 +42,13 @@ def test_contraction_inclusion():
     #pass if there are no new beliefs right?
     agent = Agent([('p'),('q'), ('p>>q'),('r')])
     new_belief = Belief('r')
+    a: int
+    a=0
     agent.belief_base.contract(new_belief)
-    
-    pass
+    for key, belief in agent.belief_base.beliefBase.items():
+        if key!='p' or key!='q' or key!='p>>q' or key!='r':
+            a=a+1
+    assert len(agent.belief_base.beliefBase) == a
 
 #test that the base didn't change
 def test_contraction_vacuity():
@@ -55,21 +56,23 @@ def test_contraction_vacuity():
     #pass if there is no change.
     agent = Agent([('p'),('q'), ('p>>q')])
     new_belief = Belief('r')
+    copy = agent.belief_base.__copy__()
     agent.belief_base.contract(new_belief)
     
-    pass
-
+    assert copy == agent.belief_base.beliefBase
 
 def test_contraction_extensionality():
     #do 10 times
     #knowledge base is p, p<->q,r contract p<->q : Cn(p,r)
     #do once knowledge base is p, p<->q,r contract p->q & q->p : Cn(p,r)
-    for x in range(10):
-        agent = Agent([('p'),('q'), ('p>>q'),('r')])
-        new_belief = Belief('q')
-        agent.belief_base.contract(new_belief)
+
+    agent = Agent([('p'),('q'), ('p>>q'),('r')])
+    agent2 = Agent([('p'),('q'), ('p>>q'),('r')])
+    new_belief = Belief('q')
+    agent.belief_base.contract(new_belief)
+    agent2.belief_base.contract(new_belief)
     #pass if all created belief bases are equivalent
-    pass
+    assert agent.belief_base.beliefBase == agent2.belief_base.beliefBase
 
 #it's not really possible to test recovery but our function should by definition make sure this postulate holds true.
 def test_contraction_recovery():
@@ -78,18 +81,30 @@ def test_contraction_recovery():
 
 #if we add a belief there should be no contradictions in the knowledge base.
 def test_revision_closure():
-    
-    pass
+    agent = Agent([('p'),('q'), ('p>>q'),('r')])
+    new_belief = Belief('~q')
+    agent.belief_base.revise(new_belief)
+    assert agent.belief_base.resolution(Not(new_belief))   
 
 #beliefs are added succesfully.
 def test_revision_success():
     
-    pass
+    agent = Agent([('p'),('q'), ('p>>q'),('r')])
+    new_belief = Belief('~r')
+    agent.belief_base.revise(new_belief)
+    assert '~r' in agent.belief_base.beliefBase
 
 #the outcome is a subset of the union between starting belief base and the new belief.
 def test_revision_inclusion():
-    
-    pass
+    agent = Agent([('p'),('q'), ('p>>q')])
+    new_belief = Belief('r')
+    a: int
+    a=0
+    agent.belief_base.revise(new_belief)
+    for key, belief in agent.belief_base.beliefBase.items():
+        if key!='p' or key!='q' or key!='p>>q' or key!='r':
+            a=a+1
+    assert len(agent.belief_base.beliefBase) == a
 
 #nothing should be removed if none of the current beliefs contradict the new belief.
 def test_revision_vacuity():
